b7fe80b3a070e95ea82065b7a20029b0e8fcd15b
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-06-06 14:47:21 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+function Calculadora() {
+    this.display = document.querySelector(".display");
+    this.capturaClick = () => {
+        document.addEventListener("click", (e) => {
+            const el = e.target;
+
+            if (el.classList.contains("btn-num")) {
+                this.addNumDisplay(el)
+            }
+            if (el.classList.contains("btn-clear")) {
+                this.clear()
+            }
+            if (el.classList.contains("btn-del")) {
+                this.del()
+            }
+            if (el.classList.contains("btn-eq")) {
+                this.realizaConta(el)
+            }
+        })
+    }
+    this.clear = () => {
+        this.display.value = ""
+    }
+
+    this.del = () => {
+        this.display.value = this.display.value.slice(0, -1)
+    };
+
+    this.realizaConta = () => {
+        try {
+            const valor = eval(this.display.value)
+
+            if (valor === String) {
+                alert("conta invalida")
+                return
+            }
+
+            this.display.value = valor
+        }
+        catch (e) {
+            this.clear();
+            alert("conta invalida")
+            return
+
+        }
+
+    };
+
+    this.addNumDisplay = (el) => {
+        this.display.value += el.innerText;
+        this.display.focus()
+
+    };
+
+    this.capturaEnter= ()=>{
+        this.display.addEventListener("keyup", (e)=>{
+            if(e.keyCode === 13){
+                this.realizaConta()
+            }
+        })
+    }
+
+    this.inicia = () => {
+        this.capturaClick();
+        this.capturaEnter();
+
+    }
+
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
